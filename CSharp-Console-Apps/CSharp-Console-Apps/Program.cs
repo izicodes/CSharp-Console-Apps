1a4f6cb569c3eccb0790f12ab03b1cf955bf8559
Isabelle Zennah <izennah@griffithsandarmour.com>
2023-03-01 20:06:23 +0000

    Working on the error handling if wrong number entered

--- CSharp-Console-Apps/CSharp-Console-Apps/Program.cs
+++ CSharp-Console-Apps/CSharp-Console-Apps/Program.cs
@@ -1,6 +1,9 @@
-﻿
+﻿using System.Threading;
 class Program
 {
+
+    string[] programs = { "HelloWorld", "Calculator", "TemperatureConverter", "InterestCalculator", "GradeCalculator", "ArraySumCalculator" };
+
     static void Main(string[] args)
     {
         MainMenu();
@@ -8,21 +11,47 @@ class Program
 
     private static void MainMenu()
     {
+        IntroductionPart();
+
+        bool continueLoop = true;
+
+        while (continueLoop)
+        {
+            Console.WriteLine("\nPick a program option:");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            if (choice < 1 || choice > 6)
+            {
+                Console.WriteLine("\nPlease pick a number from the list above.");
+                Thread.Sleep(1500);
+                Console.Clear();
+                MainMenu();
+                return;
+            }
+            else if (choice == 1){
+                Console.WriteLine("\nHello!");
+                continueLoop = false;
+            }
+        }
+
+
+
+
+
+
+    }
+
+    private static void IntroductionPart()
+    {
         Console.WriteLine(">> | MAIN MENU | <<\n");
         Console.WriteLine("Hello and welcome to XiaCodes' C# Console Applications Collection!");
-        Console.WriteLine("Pick a program option: \n");
+        Console.WriteLine("Here are the available programs: \n");
 
         string[] programs = { "HelloWorld", "Calculator", "TemperatureConverter", "InterestCalculator", "GradeCalculator", "ArraySumCalculator" };
         for (int i = 0; i < programs.Length; i++)
         {
-            Console.WriteLine($"[ {i} ] {programs[i]}\n");
+            int num = i + 1;
+            Console.WriteLine($"[ {num} ] {programs[i]}\n");
         }
-
-
-        int choice = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine(choice.ToString());
-
-
-
     }
 }
