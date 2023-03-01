f9b530d41c85d1a9236aaf3d468feebaba2c5686
Isabelle Zennah <izennah@griffithsandarmour.com>
2023-03-01 19:46:19 +0000

    Working in the MainMenu method

--- CSharp-Console-Apps/CSharp-Console-Apps/Program.cs
+++ CSharp-Console-Apps/CSharp-Console-Apps/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        MainMenu();
+    }
+
+    private static void MainMenu()
+    {
+        Console.WriteLine(">> | MAIN MENU | <<\n");
+        Console.WriteLine("Hello and welcome to XiaCodes' C# Console Applications Collection!");
+        Console.WriteLine("Pick a program option: \n");
+
+        string[] programs = { "HelloWorld", "Calculator", "TemperatureConverter", "InterestCalculator", "GradeCalculator", "ArraySumCalculator" };
+        for (int i = 0; i < programs.Length; i++)
+        {
+            Console.WriteLine($"[ {i} ] {programs[i]}\n");
+        }
+
+
+        int choice = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(choice.ToString());
+
+
+
+    }
+}
